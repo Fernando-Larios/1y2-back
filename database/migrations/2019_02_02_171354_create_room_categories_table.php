d20848db33dfc426c6cc396e8b92e3d3254c8cd0
Hosmel Quintana <hosmelq@gmail.com>
2019-02-02 11:31:58 -0600

    add base models and migrations

--- database/migrations/2019_02_02_171354_create_room_categories_table.php
+++ database/migrations/2019_02_02_171354_create_room_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoomCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('room_categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->enum('gender', ['female', 'male']);
             $table->string('name');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('room_categories');
     }
 }
--- database/migrations/2019_02_02_172223_create_reservation_queues_table.php
+++ database/migrations/2019_02_02_172223_create_reservation_queues_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateReservationQueuesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservation_queues', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->enum('gender', ['female', 'male']);
-            $table->string('name');
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('room_id')->nullable();
+            $table->foreign('room_id')->on('rooms')->references('id')->onDelete('cascade');
+            $table->morphs('reservable');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservation_queues');
     }
 }
