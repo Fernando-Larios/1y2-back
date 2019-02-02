4e2cf68799e3d0b1d60c4a1f06c24a9598801503
Hosmel Quintana <hosmelq@gmail.com>
2019-02-02 13:45:51 -0600

    update types

--- database/factories/ReservationQueueFactory.php
+++ database/factories/ReservationQueueFactory.php
@@ -4,5 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\ReservationQueue::class, function (Faker $faker) {
     return [
+        'priority' => $faker->numberBetween(1, 2),
     ];
 });
